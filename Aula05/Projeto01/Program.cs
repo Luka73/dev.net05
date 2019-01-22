5255e7d92dce41a2e5c8ca6b24f522f857e42dc3
Luana <luanafernandes73@gmail.com>
2019-01-22 21:59:10 -0200

    Construindo o CRUD de Produto

--- Aula05/Projeto01/Program.cs
+++ Aula05/Projeto01/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projeto01.Entities;
+using Projeto01.Repositories;
 
 namespace Projeto01
 {
@@ -10,6 +12,33 @@ namespace Projeto01
     {
         static void Main(string[] args)
         {
+            Produto produto = new Produto();
+            ProdutoRepository repository = new ProdutoRepository();
+
+            try
+            {
+                Console.WriteLine("Nome do Produto....: ");
+                produto.Nome = Console.ReadLine();
+
+                Console.WriteLine("Preço do Produto...: ");
+                produto.Preco = decimal.Parse(Console.ReadLine());
+
+                repository.AbrirConexao();
+                repository.Inserir(produto);
+
+                Console.WriteLine("\nProduto cadastrado com sucesso!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nErro: " + e.Message);
+            }
+            finally
+            {
+                repository.FecharConexao();
+            }
+
+
+
         }
     }
 }
